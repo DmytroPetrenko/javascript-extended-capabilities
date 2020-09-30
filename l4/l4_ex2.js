9c77bb5a15d8f2b56364ced7dd3730e88e5f0c1f
Dmytro Petrenko <dmitry.a.petrenko@gmail.com>
2020-09-30 16:49:59 +0300

    l4 ex2 map done

--- l4/l4_ex2.js
+++ l4/l4_ex2.js
@@ -3,18 +3,14 @@ window.addEventListener(
   () => document.querySelector("button").addEventListener("click", init, false),
   false
 );
-let users = new Set();
+
+let users = new Map();
+
 function init(evt) {
   console.log("Init start");
 
-  let form = document.querySelector("form")
-  users.add({
-    name: form[0].value,
-    surname: form[1].value,
-    birthDate: form[2].value,
-    spec: form[3].value,
-  });
-
+  users.set(document.querySelector("form")[2].value, getUser());
+  clearForm();
   console.log(users);
 
   evt.preventDefault();
@@ -26,9 +22,40 @@ function getUser() {
   let user = {
     name: form[0].value,
     surname: form[1].value,
-    birthDate: form[2].value,
-    spec: form[3].value,
+    email: form[2].value,
+    birthDate: form[3].value,
+    spec: form[4].value,
   };
 
   return user;
+}
+
+function clearForm() {
+  let form = document.querySelector("form");
+  for (let i = 0; i < form.length; i++) {
+    const element = form[i];
+    element.value = "";
+  }
+}
+
+window.addEventListener(
+  "load",
+  () =>
+    document.querySelector("#search").addEventListener("click", search, false),
+  false
+);
+
+function search(evt) {
+  console.log("search start");
+  let user = users.get(document.querySelector("#inputEmail2").value);
+  console.log("user found");
+
+  for (const key in user) {
+    if (user.hasOwnProperty(key)) {
+      const element = user[key];
+      document.querySelector("#output").innerHTML += `<td> ${element} </td>`;
+    }
+  }
+
+  evt.preventDefault();
 }
